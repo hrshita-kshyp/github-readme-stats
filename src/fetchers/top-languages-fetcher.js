44e04181deeaf12962436cb3dae6eb12cf150042
Harshita <85073963+hrshita-kshyp@users.noreply.github.com>
2021-08-14 10:31:34 +0530

    Update top-languages-fetcher.js

--- src/fetchers/top-languages-fetcher.js
+++ src/fetchers/top-languages-fetcher.js
@@ -6,24 +6,27 @@ const fetcher = (variables, token) => {
   return request(
     {
       query: `
-      query userInfo($login: String!) {
-        user(login: $login) {
-          repositories(isFork: false, first: 100) {
-            nodes {
-              languages(first: 1) {
-                edges {
-                  size
-                  node {
-                    color
-                    name
-                  }
-                }
-              }
+      query 
+      
+       { 
+       user(login: "<hrshita-kshyp>") {
+       repositories(ownerAffiliations: OWNER, isFork: false, first: 100) {
+       nodes {
+        name
+        languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
+          edges {
+            size
+            node {
+              color
+              name
             }
           }
         }
       }
-      `,
+    }
+  }
+}
+     
       variables,
     },
     {
